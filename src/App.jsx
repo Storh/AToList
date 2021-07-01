6b944e34c2018b9f838893286953d1a973507899
storh1118 <storh1118@outlook.com>
2021-07-01 21:47:44 +0800

    有状态组件

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,39 @@ class HelloMessage extends React.Component {
     );
   }
 }
+// 简单组件,被传入的数据可在组件中通过 this.props 在 render() 访问。
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+  }
+
+  tick() {
+    this.setState(state => ({
+      seconds: state.seconds + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        Seconds: {this.state.seconds}
+      </div>
+    );
+  }
+}
+// 除了使用外部数据（通过 this.props 访问）以外，
+// 组件还可以维护其内部的状态数据（通过 this.state 访问）。
+// 当组件的状态数据改变时，组件会再次调用 render() 方法重新渲染对应的标记。
 
 class App extends React.Component {
   // 定义一个继承于 React.Component 的叫APP的class组件 react如果要定义 class 组件，需要继承 React.Component
@@ -32,7 +65,7 @@ class App extends React.Component {
             Learn React
           </a>
           <HelloMessage name="Storh" />
-          {/* 简单组件,被传入的数据可在组件中通过 this.props 在 render() 访问。 */}
+          <Timer />
         </header>
       </div>
     );
