107c7eba2976d590dae4652c350d63657e48bd3e
storh1118 <storh1118@outlook.com>
2021-07-04 14:14:20 +0800

    调整文件结构

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
 import TodoApp from './Todo';
 
+// 简单组件,被传入的数据可在组件中通过 this.props 在 render() 访问。
 class HelloMessage extends React.Component {
   render() {
     return (
@@ -12,8 +11,10 @@ class HelloMessage extends React.Component {
     );
   }
 }
-// 简单组件,被传入的数据可在组件中通过 this.props 在 render() 访问。
 
+// 除了使用外部数据（通过 this.props 访问）以外，
+// 组件还可以维护其内部的状态数据（通过 this.state 访问）。
+// 当组件的状态数据改变时，组件会再次调用 render() 方法重新渲染对应的标记。
 class Timer extends React.Component {
   constructor(props) {
     super(props);
@@ -42,37 +43,21 @@ class Timer extends React.Component {
     );
   }
 }
-// 除了使用外部数据（通过 this.props 访问）以外，
-// 组件还可以维护其内部的状态数据（通过 this.state 访问）。
-// 当组件的状态数据改变时，组件会再次调用 render() 方法重新渲染对应的标记。
 
 class App extends React.Component {
   // 定义一个继承于 React.Component 的叫APP的class组件 react如果要定义 class 组件，需要继承 React.Component
   // Class 可以通过extends关键字实现继承
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.jsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <HelloMessage name="Storh" />
-          <Timer />
-        </header>
+      <div>
+        <HelloMessage name="Storh" />
+        <Timer />
         <TodoApp />
       </div>
     );
   }
 }
+
 // export命令定义了模块的对外接口
 // export default命令，为模块指定默认输出。
 // 使用export命令定义了模块的对外接口以后，其他 JS 文件就可以通过import命令加载这个模块。
