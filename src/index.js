9e8d158afa207c02b4a73b92fc537c6b1f3f8500
storh1118 <storh1118@outlook.com>
2021-12-01 22:02:50 +0800

    调整文件位置

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './01/App';
 import Game from './TicTacToe'
 import reportWebVitals from './reportWebVitals';
 
